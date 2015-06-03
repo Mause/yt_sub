548fe81117eab1754899884c6fd1f653908e8e8e
Dominic May <jack.thatch@gmail.com>
2015-06-04 00:18:30 +0800

    Correct default port

--- get_subscriptions.py
+++ get_subscriptions.py
@@ -210,5 +210,5 @@ def send_static(path):
 if __name__ == "__main__":
     Heroku().init_app(app)
 
-    port = os.environ.get('PORT', 9000)
+    port = os.environ.get('PORT', 5000)
     app.run(debug=True, host="0.0.0.0", port=int(port))
